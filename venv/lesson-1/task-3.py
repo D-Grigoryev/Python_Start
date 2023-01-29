dbbd43954be4602bfadccb5f5391481356f7db92
D-Grigoryev <61425345+D-Grigoryev@users.noreply.github.com>
2023-01-29 12:15:32 +0300

    Добавлены решения к 1-у заданию

--- venv/lesson-1/task-3.py
+++ venv/lesson-1/task-3.py
@@ -6,3 +6,5 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+num = input('Введите целое положительное число: ')
+print(f'Сумма чисел {num} + {num + num} + {num + num + num} = {int(num) + int(num * 2) + int(num * 3)} ')
